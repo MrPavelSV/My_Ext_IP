c783e3424daca03c22cd27a2842496e647a475c8
Pavel Spitsin <pavel.spitsin@gmail.com>
2019-04-23 19:43:52 +0700

    update

---
+++ App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
+using System;
+
+namespace Windowless_Sample
+{
+    public partial class App : Application
+    {
+        private TaskbarIcon notifyIcon;
+        private System.Threading.Timer timer;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            notifyIcon = (TaskbarIcon) FindResource("NotifyIcon");
+            notifyIcon.ToolTipText = GetIP();
+            timer = new System.Threading.Timer(OnTimedEvent);
+            timer.Change(0, 30000);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            timer.Dispose();
+            notifyIcon.Dispose();
+            base.OnExit(e);
+        }
+
+        private string GetIP()
+        {
+            string[] a = new System.IO.StreamReader(System.Net.WebRequest.Create("http://checkip.dyndns.org").GetResponse().GetResponseStream()).ReadToEnd().Trim().Split(':');
+            string a2 = a[1].Substring(1);
+            string[] a3 = a2.Split('<');
+            return a3[0];
+        }
+
+        private void OnTimedEvent(object state)
+        {
+            Dispatcher.Invoke(new Action(() =>
+            {
+                notifyIcon.ToolTipText = GetIP();
+            }
+            ));           
+        }
+    }
+}
---
+++ NotifyIconViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Windowless_Sample
+{
+    public class NotifyIconViewModel
+    {
+        public ICommand ExitApplicationCommand
+        {
+            get
+            {
+                return new DelegateCommand {CommandAction = () => Application.Current.Shutdown()};
+            }
+        }
+    }
+
+    public class DelegateCommand : ICommand
+    {
+        public Action CommandAction { get; set; }
+        public Func<bool> CanExecuteFunc { get; set; }
+
+        public void Execute(object parameter)
+        {
+            CommandAction();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteFunc == null  || CanExecuteFunc();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
